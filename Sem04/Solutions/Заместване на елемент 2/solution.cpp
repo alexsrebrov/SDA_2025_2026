82ae97842ec93e780911083ab79183e9dcaca478
Desislava Shunina <115353472+desiish@users.noreply.github.com>
2025-10-30 17:04:14 +0200

    Add getNumOnIdxX and replace methods to SinglyLinkedList
    Implement function to get value at index and replace elements in the linked list.

---
+++ Sem04/Solutions/Заместване на елемент 2/solution.cpp
@@ -0,0 +1,34 @@
+int getNumOnIdxX(int X, const SinglyLinkedList& sll)
+{
+    if(X >= sll.size)
+        return -1;
+    
+    Node* head = sll.head;
+    int pos = 0;
+    while(head)
+    {
+        if(pos == X)
+            return head->value;
+        
+        pos++;
+        head = head->next;
+    }
+    
+    return -1;
+}
+
+void SinglyLinkedList::replace(int X)
+{
+    int a = getNumOnIdxX(X, *this);
+    
+    Node* curr = head;
+    while(curr)
+    {
+        if(curr->value == a && curr->next)
+        {
+            curr->value = curr->next->value;
+        }
+        
+        curr = curr->next;
+    }
+}
