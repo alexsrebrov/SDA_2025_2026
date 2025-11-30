735e2868051754eebfa44a128025f220ba21506d
Desislava Shunina <115353472+desiish@users.noreply.github.com>
2025-11-30 15:16:13 +0200

    Create solution.cpp

---
+++ Sem08/Solutions/Unique Rows/solution.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+unordered_map<vector<size_t>, size_t> counter;
+
+int main() {
+    size_t N, M;
+    cin >> N >> M;
+    vector<vector<size_t>> arr(N, vector<size_t>(M));
+    for (size_t i = 0; i < N; ++i) {
+        for (size_t j = 0; j < M; ++j) {
+            cin >> arr[i][j];
+        }
+        counter[arr[i]]++;
+    }
+
+    size_t count = 0;
+    for (const auto& element:arr) {
+        if (counter[element] == 1) {
+            count++;
+        }
+    }
+
+    cout << count;
+    return 0;
+}
